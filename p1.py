b896337b988d59ad2e24c4e8ec0a5d7223ec6fdd
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-10-11 12:04:28 +0200

    adjustIntensity y main

--- p1.py
+++ p1.py
@@ -15,3 +15,29 @@ def show(image):
   cv.imshow("", image)
   cv.waitKey(0)
   cv.destroyAllWindows()
+
+def adjustIntensity(inImage, inRange=[], outRange=[0, 1]):
+  """
+  Altera el rango dinámico de la imagen.
+  """
+  if inRange == []:
+    min_in = np.min(inImage)
+    max_in = np.max(inImage)
+  else :
+    min_in = inRange[0]
+    max_in = inRange[1]
+  min_out = outRange[0]
+  max_out = outRange[1]
+  return min_out + (((max_out - min_out)*inImage - min_in)/(max_in - min_in))
+
+def main():
+  #
+  #  Test de adjustIntensity
+  #
+  image = read_img("./prueba/circles.png")
+  show(image)
+  image2 = adjustIntensity(image, [0, 0.5], [0, 1])
+  show(image2)
+
+if __name__ == "__main__":
+  main()
