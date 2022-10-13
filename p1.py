37513758e48eeb3dbd01df82edaaebe980d10b37
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-10-13 12:55:10 +0200

    añadidas cabeceras de las funciones y comentarios

--- p1.py
+++ p1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2 as cv
 
+# Funciones auxiliares
+
 def read_img(path):
   """
   Devuelve una imagen en punto flotante a partir de su path.
@@ -16,9 +18,13 @@ def show(image):
   cv.waitKey(0)
   cv.destroyAllWindows()
 
+# Histogramas: mejora de contraste
+
 def adjustIntensity(inImage, inRange=[], outRange=[0, 1]):
   """
   Altera el rango dinámico de la imagen.
+  - inRange = Rango de valores de intensidad de entrada.
+  - outRange = Rango de valores de intensidad de salida.
   """
   if inRange == []:
     min_in = np.min(inImage)
@@ -29,6 +35,118 @@ def adjustIntensity(inImage, inRange=[], outRange=[0, 1]):
   min_out = outRange[0]
   max_out = outRange[1]
   return min_out + (((max_out - min_out)*inImage - min_in)/(max_in - min_in))
+
+def equalizeIntensity(inImage, nBins=256):
+  """
+  Ecualiza el histograma de la imagen.
+  - nBins = número de bins empleados en el procesamiento.
+  """
+  return null
+
+# Filtrado espacial: suavizado y realce
+
+def filterImage(inImage, kernel):
+  """
+  Aplica un filtro mediante convolución de un kernel sobre una imagen.
+  - kernel = array/matriz de coeficientes
+  """
+  return null
+
+def gaussKernel1D(sigma):
+  """
+  Genera un array/matriz de una dimensión con una distribución
+  gaussiana en base a la sigma dada.
+  - sigma = desviación típica
+  """
+  return null
+
+def gaussianFilter(inImage, sigma):
+  """
+  Aplica un filtro de suavizado gaussiano sobre una imagen.
+  El kernel del filtro viene dado por el sigma que genera la
+  distribución de los coeficientes.
+  Como el filtro es linealmente separable la matriz de coeficientes
+  se puede generar multiplicando una matriz Nx1 por su matriz transpuesta.
+  Pasos: 
+    -> Multiplicar un kernel gaussiano 1xN por su matriz transpuesta
+    -> Aplicar filterImage con la matriz anterior como kernel del filtro.
+  """
+  return null
+
+def medianFilter(inImage, filterSize):
+  """
+  Suaviza una imagen mediante un filtro de medianas bidimensional. 
+  El tamaño del kernel del filtro viene dado por filterSize.
+  """
+  return null
+
+def highBoost(inImage, A, method, param):
+  """
+  Aplica el algoritmo de realce high boost.
+  - A: factor de amplificación del filtro.
+  - method: método de suavizado inicial
+    - gaussian: filtro gaussiano
+    - median: filtro de mediana
+  - param: valor del parámetro del filtro de suavizado.
+    -> sigma para gaussiano y size para medianas.
+  """
+  return null
+
+# Operadores morfológicos
+
+def erode(inImage, SE, center=[]):
+  """
+
+  """
+  return null
+
+def dilate(inImage, SE, center=[]):
+  """
+
+  """
+  return null
+
+def opening(inImage, SE, center=[]):
+  """
+
+  """
+  return null
+
+def closing(inImage, SE, center=[]):
+  """
+
+  """
+  return null
+
+def fill(inImage, seeds, SE=[], center=[]):
+  """
+
+  """
+  return null
+
+# Detección de bordes
+
+def gradientImage(inImage, operator):
+  """
+
+  """
+  return null
+
+def edgeCanny(inImage, sigma, tlow, thigh):
+  """
+
+  """
+  return null
+
+# Operación opcional
+
+def cornerHarris(inImage, sigmaD, sigmaI, t):
+  """
+
+  """
+  return null
+
+
 
 def main():
   #
