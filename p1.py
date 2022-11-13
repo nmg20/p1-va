69317f8bb344ac64019a9327ade5ac6925a0a56f
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-11-13 22:05:55 +0100

    versión simplificada de gradientImage usando filterImage

--- p1.py
+++ p1.py
@@ -339,24 +339,18 @@ def gradientImage(inImage, operator):
   a = np.array([[-1,0,1]])
   b = np.array([[1,1,1]])
   c = np.array([[1,2,1]])
-  d = np.array([[1,0,1]])
+  d = np.array([[0,1,0]])
+  
   if operator == "Roberts":
     mx, my = np.array([[-1,0],[0,1]]), np.array([[0,-1],[1,0]])
   elif operator == "CentralDiff":
-    mx, my = a.T*a, a.T*a
+    mx, my = a.T * d, d.T * a
   elif operator == "Prewitt":
     mx, my = b.T * a, a.T * b
-  elif operator == "sSobel":
+  elif operator == "Sobel":
     mx, my = c.T * a, a.T*c
-  p, q = mx.shape # Se toman las dimensiones de una matriz indistinta de las dos para hacer padding
-  a, b = p//2, q//2
-  pad = cv.copyMakeBorder(inImage,a,a,b,b,cv.BORDER_CONSTANT)
-  for x in range(a,m+a,1):
-    for y in range(b,n+b,1):
-      # Cogemos la ventana de la imagen paddeada
-      window = pad[(x-a):(x+p-a),(y-b):(y+q-b)]
-      gx[x-a,y-b] = (window*mx).sum() # Cambiar por vectores para hacer más eficiente
-      gy[x-a,y-b] = (window*my).sum() # -> matrices linealmente separables
+  gx = filterImage(inImage, mx)
+  gy = filterImage(inImage, my)
   return [gx, gy]
 
 def edgeCanny(inImage, sigma, tlow, thigh):
@@ -504,11 +498,11 @@ def main():
   #
   # image = read_img("./imagenes/morphology/closed.png")
   # image = read_img("./imagenes/grad7.png")
-  # image = read_img("./imagenes/lena.png")
-  # gx, gy = gradientImage(image, "roberts")
-  # gx = adjustIntensity(gx, [], [0,1])
+  image = read_img("./imagenes/lena.png")
+  gx, gy = gradientImage(image, "Roberts")
+  gx = adjustIntensity(gx, [], [0,1])
   # gy = adjustIntensity(gy, [], [0,1])
-  # show(image, gx)
+  show(image, gx)
   # show(image, gy)
 
 if __name__ == "__main__":
