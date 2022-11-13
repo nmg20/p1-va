c34b6c4e53c3c4d713efc65830de1033e7216975
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-11-13 21:41:33 +0100

    añadida función para operador CentralDiff en gradientImage(preguntar)

--- p1.py
+++ p1.py
@@ -326,8 +326,33 @@ def gradientImage(inImage, operator):
   Devuelve las componentes Gx y Gy del gradiente de una imagen.
     - operator: define el método que se usa para obtener el gradiente.
       -->Roberts, CentralDiff(de Prewitt/Sobel), Prewitt, Sobel.
+  Según el operador escoger una matriz a modo de SE
+    -> aplicarla convolucionalmente por la imagen
   """
-  return null
+  m, n = inImage.shape
+  gx, gy = np.zeros((m,n), dtype='float32'), np.zeros((m,n), dtype='float32')
+  a = np.array([[-1,0,1]])
+  b = np.array([[1,1,1]])
+  c = np.array([[1,2,1]])
+  d = np.array([[1,0,1]])
+  if operator == "Roberts":
+    mx, my = np.array([[-1,0],[0,1]]), np.array([[0,-1],[1,0]])
+  elif operator == "CentralDiff":
+    mx, my = a.T*a, a.T*a
+  elif operator == "Prewitt":
+    mx, my = b.T * a, a.T * b
+  elif operator == "sSobel":
+    mx, my = c.T * a, a.T*c
+  p, q = mx.shape # Se toman las dimensiones de una matriz indistinta de las dos para hacer padding
+  a, b = p//2, q//2
+  pad = cv.copyMakeBorder(inImage,a,a,b,b,cv.BORDER_CONSTANT)
+  for x in range(a,m+a,1):
+    for y in range(b,n+b,1):
+      # Cogemos la ventana de la imagen paddeada
+      window = pad[(x-a):(x+p-a),(y-b):(y+q-b)]
+      gx[x-a,y-b] = (window*mx).sum() # Cambiar por vectores para hacer más eficiente
+      gy[x-a,y-b] = (window*my).sum() # -> matrices linealmente separables
+  return [gx, gy]
 
 def edgeCanny(inImage, sigma, tlow, thigh):
   """
