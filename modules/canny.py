91656f156064288cf56d58ce1bb09e1d2877ea09
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-11-18 12:50:31 +0100

    versión completa (falta hacer pruebas y revisar histéresis)

--- modules/canny.py
+++ modules/canny.py
@@ -46,20 +46,37 @@ def suprNoMax(mags, dirs):
       # Diagonal Descendente
       elif (112.5<dirs[i,j]<157.5):
         n1, n2 = mags[i+1,j-1], mags[i-1,j+1]
+      # Comprobar si es máximo
       if (mags[i,j]>n1 and mags[i,j]>n2):
         sup[i,j]=mags[i,j]
   return sup
 
-def umbralizacion(sup, tlow, thigh)
-  
-
-def histeresis()
+def umbralizacionConHisteresis(sup, tlow, thigh):
+  """
+  Se obtiene una imagen umbralizada a partir del resultado de aplicar
+  la supresión no máxima.
+  Se aunan todos los bordes, juntando los débiles con los fuertes si están 4-conectados.
+  """
+  m, n = sup.shape
+  # Imagen con padding para no coger puntos fuera de la imagen
+  supAux = cv.copyMakeBorder(sup,1,1,1,1,cv.BORDER_CONSTANT,value=0.0)
+  umbr = np.zeros((m,n), dtype='float32')
+  for i in range(1,m,1):
+    for j in range(1,n,1):
+      # Se comprueba si el valor sobrepasa el umbral
+      if (supAux[i,j]>thigh):
+        umbr[i-1,j-1]=1.0
+      # Sino, se comprueba si está 4-conectado a un valor que sobrepase thigh
+      elif (supAux[i,j]>tlow) and ((supAux[i+1,j]+supAux[i-1,j]+supAux[i,j+1]+supAux[i,j-1])>0):
+        umbr[i-1,j-1]=1.0
+  return umbr
 
 
 def edgeCanny(inImage, sigma, tlow, thigh):
-
-
-  return null
+  direcciones, magnitudes = mejora(inImage,sigma)
+  supresion = suprNoMax(magnitudes, direcciones)
+  umbralizado = umbralizacionConHisteresis(supresion,tlow, thigh)
+  return umbralizado
 
 
 #####
@@ -69,11 +86,8 @@ def main():
   # image = p1.read_img("../imagenes/grad7.png")
   image = p1.read_img("../imagenes/lena.png")
 
-  # gx, gy = p1.gradientImage(image, "CentralDiff")
-  # gx = p1.adjustIntensity(gx, [], [0,1])
-  # gy = p1.adjustIntensity(gy, [], [0,1])
-  # p1.show(image, gx)
-  # p1.show(image, gy)
+  img = edgeCanny(image, 1.5, 0.2, 0.5)
+  p1.show(image, img)
 
 if __name__ == "__main__":
   main()
