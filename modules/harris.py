f588f1d9c30e378dca5f66706afc8a2ea0a3f7bb
Nicolás Martínez González <n.gonzalezm@udc.es>
2022-11-22 20:15:19 +0100

    función de detección de esquinas Harris en pruebas (funcional)

--- modules/harris.py
+++ modules/harris.py
@@ -32,7 +32,19 @@ def suprNoMax(mags, dirs):
   return sup
 
 def cornerHarris(inImage, sigmaD, sigmaI, t):
-  fr, fc = p1.gradientImage(inImage,"Sobel")
+  # m, n = inImage.shape
+  gx, gy = p1.gradientImage(inImage,"Sobel")
+  gdx2 = p1.gaussianFilter(gx**2,sigmaD)
+  gdy2 = p1.gaussianFilter(gy**2,sigmaD)
+  gdxy = p1.gaussianFilter(gx*gy,sigmaD)
+  # detA = ixx * iyy - ixy**2
+  # trace = ixx + iyy
+  # response = detA - k * trace ** 2
+  harris = gdx2*gdy2 - gdxy**2 - 0.12*(gdx2+gdy2)**2
+  harris = p1.adjustIntensity(harris,[],[0,1])
+
+
+
   return outCorners, harrisMap
 
 
